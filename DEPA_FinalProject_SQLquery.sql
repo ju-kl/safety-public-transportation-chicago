8545c10033c96c5a190fbc1e8ed60964075dfa41
Jainam <jainam1995@gmail.com>
2019-12-04 14:30:39 -0600

    Updated DataPrep file and SQLQuery

--- DEPA_FinalProject_SQLquery.sql
+++ DEPA_FinalProject_SQLquery.sql
@@ -1,7 +1,21 @@
+/***********************************************
+** GROUP 6 FINAL PROJECT
+** DATA ENGINEERING PLATFORMS (MSCA 31012)
+
+** File: DEPA_FinalProject_SQLquery.sql
+** Desc: Verifying and querying our data from CloudSQL
+** Auth: Jainam Mehta, Julian Kleindiek, Lola Johnston, Peter Eusebio
+** Date: 04 Dec 2019
+** ALL RIGHTS RESERVED | DO NOT DISTRIBUTE
+************************************************/
+
+# Select the mydb database, where our data is located
 USE `mydb`;
+
+# Show the list of tables
 SHOW TABLES;
 
-########## Check that data is populated into different tables
+########### Verify that all data has been populated correctly ##########
 
 SELECT COUNT(*) FROM crime;
 SELECT * FROM crime
@@ -15,6 +29,7 @@ SELECT COUNT(*) FROM TrainStops;
 SELECT * FROM TrainStops
 LIMIT 10;
 
+
 SELECT COUNT(*) FROM grid;
 SELECT * FROM grid
 LIMIT 10;
@@ -26,6 +41,18 @@ LIMIT 10;
 SELECT COUNT(*) FROM weather;
 SELECT * FROM weather
 LIMIT 10;
+
+## Checking the datatype of a certain column
+SELECT DATA_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE 
+     TABLE_NAME = 'BusStops' AND 
+     COLUMN_NAME = 'systemStop';
+
+########### Querying the data ##########
+
+
+
 
 ############################################
 SET SQL_SAFE_UPDATES = 0;

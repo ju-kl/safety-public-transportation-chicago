121a934cb9fb11d8d992c6db3243f2900f74a850
Jainam <jainam1995@gmail.com>
2019-12-05 17:05:59 -0600

    Update DEPA_FinalProject_SQLquery.sql

--- DEPA_FinalProject_SQLquery.sql
+++ DEPA_FinalProject_SQLquery.sql
@@ -15,47 +15,103 @@ USE `mydb`;
 # Show the list of tables
 SHOW TABLES;
 
+
 ########### Verify that all data has been populated correctly ##########
 
+#### Get the record count and view the data for each table
+
+# Crime table
 SELECT COUNT(*) FROM crime;
 SELECT * FROM crime
 LIMIT 10;
 
+# Bus Stops table
 SELECT COUNT(*) FROM BusStops;
 SELECT * FROM BusStops
 LIMIT 10;
 
+# Train stops table
 SELECT COUNT(*) FROM TrainStops;
 SELECT * FROM TrainStops
 LIMIT 10;
 
+# Grid table
 SELECT COUNT(*) FROM grid;
 SELECT * FROM grid
 LIMIT 10;
 
+# Holiday table
 SELECT COUNT(*) FROM hday;
 SELECT * FROM hday
 LIMIT 10;
 
+# Weather table
 SELECT COUNT(*) FROM weather;
 SELECT * FROM weather
 LIMIT 10;
 
-## Check the datatype of a certain column
-SELECT DATA_TYPE 
+## Check the datatypes from a certain column
+SELECT COLUMN_NAME, DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE 
-     TABLE_NAME = 'BusStops' AND 
-     COLUMN_NAME = 'systemStop';
+WHERE TABLE_NAME = 'TrainStops';
+
 
 ########### Querying the data ##########
 
 # Find the number of crimes ocurring by Location type
-SELECT 
-    locationDescription, COUNT(*)
-FROM
-    crime
+SELECT locationDescription, COUNT(*) AS numberOfCrimes
+FROM crime
 GROUP BY locationDescription;
+
+# Find the total number of crimes occuring per year
+SELECT year, COUNT(*) AS numberOfCrimes
+FROM crime
+GROUP BY year;
+
+# Find the top 5 types of crime
+SELECT primaryType, COUNT(*) AS numberOfCrimes
+FROM crime
+GROUP BY primaryType
+ORDER BY COUNT(*) DESC
+LIMIT 5;
+
+# Average number of crimes per day
+SELECT COUNT(*) / (DATEDIFF(MAX(date), MIN(date))) AS avgCrime
+from crime;
+# Average of 14.85 crimes per day
+
+# Top 5 Holidays with the most crime occurences in 2019
+SELECT Holiday, DATE(Date) as Date, COUNT(*) as numberOfCrimes
+FROM hday
+INNER JOIN crime USING (Date)
+WHERE YEAR(Date) = 2019
+GROUP BY Holiday , Date
+ORDER BY COUNT(*) DESC
+LIMIT 5;
+# Most number of crimes occured on Memorial day, which was 20
+
+
+
+
+
+
+# Note there are 6912 days between Jan 1 2001 to 05 Dec 2019
+SELECT *
+from crime
+order by date ASC
+limit 5;
+
+
+
+
+# Grids with the most bus stops
+
+
+
+
+
+
+
 
 
 
